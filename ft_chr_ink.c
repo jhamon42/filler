ba0a65a032211202e6b8cfcd573d002852eb198d
Jerome HAMON <jhamon@e2r11p7.42.fr>
2018-04-19 14:26:50 +0200

    update 19/04/18

--- ft_chr_ink.c
+++ ft_chr_ink.c
@@ -6,7 +6,7 @@
 /*   By: jhamon <jhamon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/05 01:34:23 by jhamon            #+#    #+#             */
-/*   Updated: 2018/04/10 17:06:10 by jhamon           ###   ########.fr       */
+/*   Updated: 2018/04/18 04:39:50 by jhamon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,69 +15,103 @@
 static void	ft_phaseur_ion(t_filler *p)
 {
 	if (p->phase == 1)
-		p->phase = 1;
+		p->phase = 9;
 	else if (p->phase == 2)
-		p->phase = 2;
+		p->phase = 10;
 	else if (p->phase == 3)
-		p->phase = 6;
+		p->phase = 7;
 	else if (p->phase == 4)
-		p->phase = 5;
+		p->phase = 8;
 	else if (p->phase == 5)
-		p->phase = 4;
+		p->phase = 6;
 	else if (p->phase == 6)
+		p->phase = 5;
+	else if (p->phase == 7)
 		p->phase = 3;
+	else if (p->phase == 8)
+		p->phase = 4;
+	else if (p->phase == 9)
+		p->phase = 1;
+	else if (p->phase == 10)
+		p->phase = 2;
 }
 
 static void	ft_phaseur_elec(t_filler *p)
 {
 	if (p->phase == 1)
-		p->phase = 6;
+		p->phase = -9;
 	else if (p->phase == 2)
-		p->phase = 3;
+		p->phase = -10;
 	else if (p->phase == 3)
-		p->phase = 9;
+		p->phase = -7;
 	else if (p->phase == 4)
-		p->phase = 9;
+		p->phase = -8;
 	else if (p->phase == 5)
-		p->phase = 9;
+		p->phase = -6;
 	else if (p->phase == 6)
-		p->phase = 9;
+		p->phase = -5;
 	else if (p->phase == 7)
-		p->phase = 4;
+		p->phase = -3;
 	else if (p->phase == 8)
-		p->phase = 5;
+		p->phase = -4;
+	else if (p->phase == 9)
+		p->phase = -1;
+	else if (p->phase == 10)
+		p->phase = -2;
+	else if (p->phase == 11)
+		p->phase = 13;
+	else if (p->phase == 12)
+		p->phase = 13;
 }
 
 static void	ft_phaseur_negatron(t_filler *p)
 {
 	if (p->phase == -1)
-		p->phase = 7;
+		p->phase = 13;
 	else if (p->phase == -2)
-		p->phase = 7;
+		p->phase = 13;
 	else if (p->phase == -3)
-		p->phase = 1;
+		p->phase = 12;
 	else if (p->phase == -4)
-		p->phase = 7;
+		p->phase = 11;
 	else if (p->phase == -5)
-		p->phase = 7;
+		p->phase = 12;
 	else if (p->phase == -6)
-		p->phase = 2;
+		p->phase = 11;
+	else if (p->phase == -7)
+		p->phase = 11;
+	else if (p->phase == -8)
+		p->phase = 12;
+	else if (p->phase == -9)
+		p->phase = 11;
+	else if (p->phase == -10)
+		p->phase = 12;
 }
 
 static void	ft_phaseur_netron(t_filler *p)
 {
 	if (p->phase == 1)
-		p->phase = 6;
+		p->phase = 13;
 	else if (p->phase == 2)
-		p->phase = 3;
+		p->phase = 13;
 	else if (p->phase == 3)
-		p->phase = 9;
+		p->phase = 1;
 	else if (p->phase == 4)
 		p->phase = 2;
 	else if (p->phase == 5)
-		p->phase = 1;
+		p->phase = 3;
 	else if (p->phase == 6)
-		p->phase = 9;
+		p->phase = 4;
+	else if (p->phase == 7)
+		p->phase = 6;
+	else if (p->phase == 8)
+		p->phase = 5;
+	else if (p->phase == 9)
+		p->phase = 7;
+	else if (p->phase == 10)
+		p->phase = 8;
+	else
+		p->phase = 13;
 }
 
 static int	ft_awesome_rchr(t_filler *p, char c)
@@ -94,13 +128,24 @@ static int	ft_awesome_rchr(t_filler *p, char c)
 	if ((x + 1 == p->x_map && y == 0) || (y + 1 == p->y_map && x == 0) || \
 		(x == 0 && y == 0) || (x + 1 == p->x_map && y + 1 == p->y_map))
 		return (ft_cas_prt(p, c));
-	else if (f == 1 || f == 5 || f == 6 || f == -5 || f == -6 || f == -1 || f == 8)
+	else if (f % 2 == 0)
 	{
 		if (p->tb_map[y - 1][x] == c)
 			if (p->tb_map[y + 1][x] == c)
-				return (1);
+			{
+				if (f >= -6 && f <= 6)
+				{
+					if (p->tb_map[y][x - 1] == c)
+						return (1);
+				}
+				else
+				{
+					if (p->tb_map[y][x + 1] == c)
+						return (1);
+				}
+			}
 	}
-	else if (f == 2 || f == 4 || f == 3 || f == -4 || f == -3 || f == -2 || f == 7)
+	else if (f % 2 == 1)
 	{
 		if (p->tb_map[y][x - 1] == c)
 			if (p->tb_map[y][x + 1] == c)
@@ -111,12 +156,12 @@ static int	ft_awesome_rchr(t_filler *p, char c)
 
 void	ft_chr_ink(t_filler *p)
 {
-	if (p->phase != 9)
+	if (p->phase != 13)
 	{
 		if (ft_awesome_rchr(p, '.'))
 		{
-			if (p->phase > 0 && p->phase < 7)
-				;//ft_phaseur_ion(p);
+			if (p->phase > 0)
+				ft_phaseur_ion(p);
 		}
 		else
 		{
@@ -124,8 +169,8 @@ void	ft_chr_ink(t_filler *p)
 			{
 				if (p->phase > 0)
 					ft_phaseur_elec(p);
-				// else
-				// 	ft_phaseur_negatron(p);
+				else
+				 	ft_phaseur_negatron(p);
 			}
 			else
 				ft_phaseur_netron(p);
--- ft_hercul.c
+++ ft_hercul.c
@@ -6,11 +6,7 @@
 /*   By: jhamon <jhamon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/30 21:07:41 by jhamon            #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2018/04/16 17:13:57 by jhamon           ###   ########.fr       */
-=======
-/*   Updated: 2018/04/10 22:04:53 by jhamon           ###   ########.fr       */
->>>>>>> 500803856512f863eef42a3c0279a1511c4d2281
+/*   Updated: 2018/04/16 19:44:06 by jhamon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,31 +14,14 @@
 
 static void	ft_medusa(t_filler *p)
 {
-	if (p->y_ini_en > p->y_ini_me)
-		;
+	p->phase = 1;
 }
 
 static void	ft_hydra(t_filler *p)
 {
-	if (p->phase == 4 || p->phase == -4)
 	{
-		p->x_ink = p->x_map / 2;
-		p->y_ink = (p->y_ini_me < (p->y_map / 2)) ? p->y_map - 1 : 0;
-	}
-	else if (p->phase == 5 || p->phase == -5)
-	{
-		p->y_ink = p->y_map / 2;
-		p->x_ink = (p->x_ini_me < (p->x_map / 2)) ? p->x_map - 1 : 0;
-	}
-	else if (p->phase == 6 || p->phase == -6)
-	{
-		p->y_ink = (p->y_ini_me > (p->y_map / 2)) ? p->y_map / 3 : (p->y_map - (p->y_map / 3));
-		p->x_ink = (p->x_ini_me >= (p->x_map / 2)) ? p->x_map - 1: 0;
-	}
-	else if (p->phase == 9)
-	{
-		p->y_ink = p->y_ini_en;
 		p->x_ink = p->x_ini_en;
+		p->y_ink = p->y_ini_en;
 	}
 }
 
@@ -52,28 +31,63 @@ void	ft_hercul(t_filler *p)
 		ft_medusa(p);
 	else if (p->phase == 1 || p->phase == -1)
 	{
-		p->y_ink = p->y_ini_me;
-		p->x_ink = (p->x_ini_me < (p->x_map / 2)) ? p->x_map - 1 : 0;
+		p->x_ink = p->x_ini_me;
+		p->y_ink = (p->y_ini_me > (p->y_map / 2)) ? p->y_map - 1 : 0;
 	}
 	else if (p->phase == 2 || p->phase == -2)
 	{
-		p->x_ink = p->x_ini_me;
-		p->y_ink = (p->y_ini_me < (p->y_map / 2)) ? p->y_map - 1 : 0;
+		p->y_ink = p->y_ini_me;
+		p->x_ink = (p->x_ini_me > (p->x_map / 2)) ? p->x_map - 1 : 0;
 	}
 	else if (p->phase == 3 || p->phase == -3)
 	{
-		p->x_ink = (p->x_ini_me > (p->x_map / 2)) ? p->x_map / 3 : (p->x_map - (p->x_map / 3));
-		p->y_ink = (p->y_ini_me >= (p->y_map / 2)) ? p->y_map - 1 : 0;
+		p->x_ink = p->x_map / 2;
+		p->y_ink = (p->y_ini_me > (p->y_map / 2)) ? p->y_map - 1 : 0;
+	}
+	else if (p->phase == 4 || p->phase == -4)
+	{
+		p->x_ink = (p->x_ini_me > (p->x_map / 2)) ? p->x_map - 1 : 0;
+		p->y_ink = p->y_map / 2;
+	}
+	else if (p->phase == 5 || p->phase == -5)
+	{
+		p->y_ink = (p->y_ini_en < (p->y_map / 2)) ? p->y_map - 1 : 0;
+		p->x_ink = p->x_ini_en;
+	}
+	else if (p->phase == 6 || p->phase == -6)
+	{
+		p->x_ink = (p->x_ini_en < (p->x_map / 2)) ? p->x_map - 1 : 0;
+		p->y_ink = p->y_ini_en;
 	}
 	else if (p->phase == 7 || p->phase == -7)
 	{
 		p->x_ink = p->x_ini_me;
-		p->y_ink = (p->y_ini_me >= (p->y_map / 2)) ? p->y_map - 1 : 0;
+		p->y_ink = (p->y_ini_me > (p->y_map / 2)) ? 0 : p->y_map - 1;
 	}
 	else if (p->phase == 8 || p->phase == -8)
 	{
 		p->y_ink = p->y_ini_me;
-		p->x_ink = (p->x_ini_me >= (p->x_map / 2)) ? p->x_map - 1 : 0;
+		p->x_ink = (p->x_ini_me > (p->x_map / 2)) ? 0 : p->x_map - 1;
+	}
+	else if (p->phase == 9 || p->phase == -9)
+	{
+		p->x_ink = p->x_map / 2;
+		p->y_ink = (p->y_ini_me > (p->y_map / 2)) ? 0 : p->y_map - 1;
+	}
+	else if (p->phase == 10 || p->phase == -10)
+	{
+		p->x_ink = (p->x_ini_me > (p->x_map / 2)) ? 0 : p->x_map - 1;
+		p->y_ink = p->y_map / 2;
+	}
+	else if (p->phase == 11 || p->phase == -11)
+	{
+		p->y_ink = (p->y_ini_en > (p->y_map / 2)) ? p->y_map - 1 : 0;
+		p->x_ink = p->x_ini_en;
+	}
+	else if (p->phase == 12 || p->phase == -12)
+	{
+		p->x_ink = (p->x_ini_en > (p->x_map / 2)) ? p->x_map - 1 : 0;
+		p->y_ink = p->y_ini_en;
 	}
 	else
 		ft_hydra(p);
